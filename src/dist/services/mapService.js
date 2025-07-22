c742063424b3c92f462d4a9d0926635fbe579ae9
Vince Quiles <vjquiles@msn.com>
2025-07-21 22:12:25 -0400

    I never thought i would be so happy to see a clg statement :,) at least i now know there is some wonkiness going on with where my ts is compiled to. actually im gonna commit this and then pop loadmap into the window event listener and see whats up

---
+++ src/dist/services/mapService.js
@@ -0,0 +1,18 @@
+// import L from 'leaflet.js';
+// import 'leaflet/dist/leaflet.css';
+let map;
+let marker;
+export function loadMap() {
+    map = L.map('map').setView([39.900833, -75.1675], 10);
+    L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=joTmYYOJlSXYIyLIMnvF', {
+        maxZoom: 19,
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    }).addTo(map);
+    marker = L.marker([39.900833, -75.1675]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    console.log('is this thing on?');
+}
+window.addEventListener('load', () => {
+    console.log('maybe this will work?');
+});
