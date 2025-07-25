c96afae9b2d5780994ed2d2eb19ccec21cf86de5
Vince Quiles <vjquiles@msn.com>
2025-07-24 20:19:49 -0400

    Created dom elements as an object for easy reference in dom event handler utility

---
+++ src/models/domCache.ts
@@ -0,0 +1,9 @@
+export const domCache = {
+    userInput: document.getElementById('user-input') as HTMLInputElement,
+    submitBtn: document.getElementById('search-btn') as HTMLButtonElement,
+
+    ipDisplay: document.getElementById('ip-display') as HTMLSpanElement,
+    cityDisplay: document.getElementById('city-display') as HTMLSpanElement,
+    tzDisplay: document.getElementById('timezone-display') as HTMLSpanElement,
+    championDisplay: document.getElementById('champion-display') as HTMLSpanElement,
+}
