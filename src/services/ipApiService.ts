e5ae04b5035159f334abe0dff16db8a1b8e108ad
Vince Quiles <vjquiles@msn.com>
2025-07-26 17:08:26 -0400

    When i went to deploy to netlify, i noticed when i tried typing in websites it didnt wrok. Had to go back to my api to ammend the request, because i coded where i only was searching for ip addresses. I created a couple checks, and then used a ternary operator to discern between both checks and return the input to move to send the rquest. Now im really done lol.

--- src/services/ipApiService.ts
+++ src/services/ipApiService.ts
@@ -1,11 +1,22 @@
 import Location from '../models/IP-Info.js'
 import { ValidationError, NetworkError, DataError } from '../utils/errorHandler.js'
 
-export async function fetchIPData(ip: string): Promise<Location> {
+export async function fetchIPData(input: string): Promise<Location> {
+
+    const ipRegex = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/
+    const domainRegex = /^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,6})+$/
+
+    const ipCheck = ipRegex.test(input)
+    const domainCheck = domainRegex.test(input)
+    const urlItem = ipCheck ? `ipAddress=${input}` : domainCheck ? `domain=${input}` : null
+
+    if (!urlItem) {
+        throw new ValidationError("Invalid entry! We ain't tryna waste no tokens")
+    }
 
     const baseURL = "https://geo.ipify.org/api/v1"
     const ipApiKey = '??'
-    const url = `${baseURL}?apiKey=${ipApiKey}&ipAddress=${ip}`
+    const url = `${baseURL}?apiKey=${ipApiKey}&${urlItem}`
 
     try {
         const response = await fetch(url)
